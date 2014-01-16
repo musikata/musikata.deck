29fe9c8f69fcf5a152adf397a0827495dac0a170
adorsk <adorsk>
2014-01-16 16:38:49 -0500

    updated test config

---
+++ test/specFiles.js
@@ -0,0 +1,27 @@
+// We specify specs individually rather than by pattern.
+// This makes it easier to switch between branches without
+// confusing Karma with missing AMD dependencies.
+
+var specFiles = [
+  'CompositeModel.spec.js',
+  'CompositeView.spec.js',
+  'DeckFactory.spec.js',
+  'DeckModel.spec.js',
+  'DeckView.spec.js',
+  'HtmlView.spec.js',
+  'ModelFactory.spec.js',
+  'SlideModel.spec.js',
+  'ViewFactory.spec.js',
+];
+
+// Prefix specs with specsDir.
+var specsDir = 'test/specs';
+specFiles.forEach(function(specFile, i){
+  specFiles[i] = {
+    pattern: specsDir + "/" + specFile,
+    watched: true,
+    included: false
+  };
+});
+
+exports.files = specFiles;
