c0460f1c4af3558c3908eb066a68f7fed865b90a
adorsk <adorsk@gmail.com>
2014-01-17 12:11:31 -0500

    fleshing out HealthView

--- src/HealthView.js
+++ src/HealthView.js
@@ -1,12 +1,49 @@
 define(
   [
-  'marionette'
+  'marionette',
+  'handlebars',
+  'text!./templates/HealthView.html'
 ],
 function(
-  Marionette
+  Marionette,
+  Handlebars,
+  HealthViewTemplate
 ){
 
   var HealthView = Marionette.ItemView.extend({
+    template: Handlebars.compile(HealthViewTemplate),
+    templateHelpers: {
+      healthUnits: function(){
+        var healthUnits = [];
+        for (var i=0; i < this.size; i++){
+          healthUnits.push({
+            status: (i < this.currentHealth) ? '' : 'disabled'
+          });
+        }
+        return healthUnits;
+      }
+    },
+
+    ui: {
+      'healthUnits': '.health_unit'
+    },
+
+    modelEvents: {
+      "change:currentHealth": "updateHealthUnits"
+    },
+
+    updateHealthUnits: function(){
+      var currentHealth = this.model.get('currentHealth');
+      this.ui.healthUnits.each(function(idx, el){
+        if (idx < currentHealth){
+          $(el).removeClass('disabled');
+        }
+        else{
+          $(el).addClass('disabled');
+        }
+      });
+    }
+
   });
 
   return HealthView;
--- test/specs/HealthView.spec.js
+++ test/specs/HealthView.spec.js
@@ -1,41 +1,53 @@
 define(
   [
   'deck/HealthView',
-  'backbone'
+  'deck/HealthModel',
 ],
 function(
   HealthView,
-  Backbone
+  HealthModel
 ){
-
-  describe('HealthView', function(){
-
-    var healthView;
-    var healthModel;
+  ddescribe('HealthView', function(){
 
     var generateHealthView = function(){
-      healthModel = new Backbone.Model({
+      var healthModel = new HealthModel({
         size: 3,
       });
+      var healthView = new HealthView({
+        model: healthModel
+      });
+      return healthView;
     };
-
-    afterEach(function(){
-      if (healthView){
-        healthView.remove();
-        healthView = null;
-      }
-    });
 
     it('should be defined', function(){
       expect(HealthView).toBeDefined();
     });
 
     it('should display correct number of health units per size', function(){
-      this.fail('TODO');
+      var view = generateHealthView();
+      view.render();
+      expect(view.$el.find('.health_unit').length).toBe(view.model.get('size'));
+      view.remove();
     });
 
     it('should update unit classes when currentHealth changes', function(){
-      this.fail('TODO');
+      var view = generateHealthView();
+      view.render();
+
+      var getActualNumDisabledUnits = function(){
+        return view.$el.find('.health_unit.disabled').length;
+      };
+
+      var getExpectedNumDisabledUnits = function(){
+        return view.model.get("size") - view.model.get("currentHealth");
+      };
+
+      expect(getActualNumDisabledUnits()).toEqual(getExpectedNumDisabledUnits());
+      view.model.set('currentHealth', 2);
+      expect(getActualNumDisabledUnits()).toEqual(getExpectedNumDisabledUnits());
+      view.model.set('currentHealth', 3);
+      expect(getActualNumDisabledUnits()).toEqual(getExpectedNumDisabledUnits());
+      view.remove();
     });
 
   });
